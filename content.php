ee6ff1b83e749421f69f17e305952184a00d4d90
Diego Caro <diegocaro@gmail.com>
2014-01-17 11:21:54 +0100

    Cambio a repositorio dedicado

---
+++ content.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" target="_blank" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		</header><!-- .entry-header -->
+
+		<footer class="entry-meta">
+			<?php $show_sep = false; ?>
+
+			<?php if ( is_object_in_taxonomy( get_post_type(), 'post_tag' ) ) : // Hide tag text when not supported ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
+				if ( $tags_list ):
+				if ( $show_sep ) : ?>
+			<span class="sep"> | </span>
+				<?php endif; // End if $show_sep ?>
+			<span class="tag-links">
+				<?php printf( __( '<span class="%1$s">Etiquetas:</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
+				$show_sep = true; ?>
+			</span>
+			<?php endif; // End if $tags_list ?>
+			<?php endif; // End if is_object_in_taxonomy( get_post_type(), 'post_tag' ) ?>
+
+			<?php if ( comments_open() ) : ?>
+			<?php if ( $show_sep ) : ?>
+			<span class="sep"> | </span>
+			<?php endif; // End if $show_sep ?>
+			<span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentyeleven' ) . '</span>', __( '<b>1</b> Reply', 'twentyeleven' ), __( '<b>%</b> Replies', 'twentyeleven' ) ); ?></span>
+			<?php endif; // End if comments_open() ?>
+
+			<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post-<?php the_ID(); ?> -->
